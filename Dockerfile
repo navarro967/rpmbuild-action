771ae07a7241cd18a7090c52c1f6babf0bce43ee
Joshua <joshua.navarro@kaltura.com>
2022-12-15 12:06:03 -0500

    refactor

--- Dockerfile
+++ Dockerfile
@@ -6,18 +6,16 @@ COPY . .
 
 # Installing tools needed for rpmbuild , 
 # depends on BuildRequires field in specfile, (TODO: take as input & install)
-RUN yum install -y rpm-build rpmdevtools gcc make coreutils python git
+RUN yum install -y epel-release
+
+RUN yum install -y rpm-build rpmdevtools gcc make coreutils python git jq
 
 # Setting up node to run our JS file
 # Download Node Linux binary
-RUN curl -O https://nodejs.org/dist/v12.16.1/node-v12.16.1-linux-x64.tar.xz
-
-# Extract and install
-RUN tar --strip-components 1 -xvf node-v* -C /usr/local
-
-# Install dependecies and build main.js
-RUN npm install --production \
-&& npm run-script build
+RUN curl -O https://nodejs.org/dist/v12.16.1/node-v12.16.1-linux-x64.tar.xz && \
+tar --strip-components 1 -xf node-v* -C /usr/local && \
+npm install --production && \
+npm run-script build
 
 # All remaining logic goes inside main.js , 
 # where we have access to both tools of this container and 
--- src/main.ts
+++ src/main.ts
@@ -12,50 +12,59 @@ async function run() {
     // Get github context data
     const context = github.context;
 
-    // To be used to get contents of this git ref 
-    const owner = context.repo.owner
-    const repo = context.repo.repo
-    const ref = context.ref
-
     // get inputs from workflow
-    let additionalDefs  = "";
-    let githubToken = "";
-    const configPath = core.getInput('spec_file'); // user input, eg: `foo.spec' or `rpm/foo.spec'
-    const basename = path.basename(configPath); // always just `foo.spec`
-    const specFile = {
-      srcFullPath: `/github/workspace/${configPath}`,
+    const inputs = {
+      additional_definitions: core.getInput('additional_definitions'),
+      additional_packages:    core.getInput('additional_packages'),
+      github_token:           core.getInput('github_token'),
+      make_version:           core.getInput('make_version'),
+      make_release:           core.getInput('make_release'),
+      make_path:              core.getInput('make_path')
+    };
+    
+    // intialize variables
+    const owner     = context.repo.owner
+    const repo      = context.repo.repo
+    const ref       = context.ref
+    const cwd       = process.cwd();
+    const basename  = path.basename(inputs.make_path); // always just `foo.spec`
+
+    const makeFile            = {
+      srcFullPath: `/github/workspace/${inputs.make_path}`,
       destFullPath: `/github/home/rpmbuild/SPECS/${basename}`,
     };
 
     console.log(`${await exec.exec("ls -alh /github/workspace/")}`);
 
-    let name = '';       
-    let version = '';
+    const name        = inputs.github_token ? 'GITHUB_TOKEN='+inputs.github_token : "";
+    const version     = inputs.github_token ? 'VERSION='+inputs.github_token : "";
+    const release     = inputs.github_token ? 'RELEASE='+inputs.github_token : "";
+    const githubToken = inputs.github_token ? 'GITHUB_TOKEN='+inputs.github_token : "";
+
 
     // Installs additional repositories
-    const additionalRepos = core.getInput('additional_repos'); // user input, eg: '["centos-release-scl"]'
+    // user input, eg: '["centos-release-scl"]'
 
-    if(core.getInput('additional_definitions')) 
-      additionalDefs = core.getInput('additional_definitions');
-
-    if(core.getInput('github_token')) 
-      githubToken = `GITHUB_TOKEN=${core.getInput('github_token')}`;
-
-    if (additionalRepos) {
-      const arr = JSON.parse(additionalRepos);
+    if (inputs.additional_packages) {
+      const arr = JSON.parse(inputs.additional_packages);
       for (let i = 0; i < arr.length; i++) {
         console.log(`Installing repo': ${arr[i]}`);
           await exec.exec(`yum install -y ${arr[i]}`);
       };
     }
 
+    process.chdir(makeFile.srcFullPath);
+
+    console.log(`${await exec.exec(`cat Makefile`)}`);
     try {
       await exec.exec(
-        `cd ${specFile.srcFullPath} && make build ${githubToken}`
+        `make build ${version} ${release} ${githubToken} ${inputs.additional_definitions}`
       );
     } catch (err) {
       core.setFailed(`action failed with error: ${err}`);
     }
+
+    process.chdir(cwd);
 
     // only contents of workspace can be changed by actions and used by subsequent actions 
     // So copy all generated rpms into workspace , and publish output path relative to workspace (/github/workspace)
